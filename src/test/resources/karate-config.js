4f04e805a7378a5dad2058d2eff03d8ea93bcf55
KuldeepYadav <kuldeep.ya9@gmail.com>
2024-12-02 00:21:30 +0530

    test

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,6 +1,10 @@
 function fn() {
-  var env = karate.env; // get system property 'karate.env'
-  karate.log('karate.env system property was:', env);
+  //var env = karate.env; // get system property 'karate.env'
+
+   var apiKey = karate.properties['apiKey']
+   var env = karate.properties['environment']
+     karate.log('karate.env system property was:', apiKey);
+     karate.log('karate.env system property was:', env);
   if (!env) {
     env = 'dev';
   }
